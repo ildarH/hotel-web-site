8d02c1b68fe2a040eee13c74f28b9235b951ce06
ildar habibrahmanov <ildar.habibrahmanov@gmail.com>
2020-09-22 20:09:20 +0700

    -m: index.html>map-yandex main.js>api map.yandexl

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@ var mySwiper = new Swiper('.swiper-container', {
     },
 
   })
+
+ymaps.ready(init);
+function init(){ 
+    let myPlacemark;
+    var myMap = new ymaps.Map("map", {
+        center: [37.78526803249929,-122.4082276146421],
+        zoom: 16
+    });
+
+  myPlacemark = new ymaps.Placemark([37.78526803249929,-122.4082276146421]), {
+    hintContent: 'Hotel',
+    balloonContent: 'Hilton Grand Hotel'
+  };
+  myMap.geoObjects.add(myPlacemark)
+}
