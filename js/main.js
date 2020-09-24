939b4f5246a918f00007e80a59a8e7aabbeaf0a1
ildar habibrahmanov <ildar.habibrahmanov@gmail.com>
2020-09-24 13:25:15 +0700

    -m: index.html>reviews block style.sass -a: reviews.sass img

--- js/main.js
+++ js/main.js
@@ -1,31 +1,46 @@
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+var hotelSlider = new Swiper('.hotel-slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.slider-button__next',
-      prevEl: '.slider-button__prev',
-    },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button-next',
+    prevEl: '.hotel-slider__button-prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+var reviewSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-  })
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button-next',
+    prevEl: '.reviews-slider__button-prev',
+  },
+  // keyboard: {
+  //   enabled: true,
+  //   onlyInViewport: false,
+  // },
+});
 
 ymaps.ready(init);
-function init(){ 
-    let myPlacemark;
-    var myMap = new ymaps.Map("map", {
-        center: [37.78526803249929,-122.4082276146421],
-        zoom: 16
-    });
+function init() {
+  let myPlacemark;
+  var myMap = new ymaps.Map('map', {
+    center: [37.78526803249929, -122.4082276146421],
+    zoom: 16,
+  });
 
-  myPlacemark = new ymaps.Placemark([37.78526803249929,-122.4082276146421]), {
-    hintContent: 'Hotel',
-    balloonContent: 'Hilton Grand Hotel'
-  };
-  myMap.geoObjects.add(myPlacemark)
+  (myPlacemark = new ymaps.Placemark([37.78526803249929, -122.4082276146421])),
+    {
+      hintContent: 'Hotel',
+      balloonContent: 'Hilton Grand Hotel',
+    };
+  myMap.geoObjects.add(myPlacemark);
 }
