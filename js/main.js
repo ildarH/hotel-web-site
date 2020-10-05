552c4d08b00a7d9e9d0b8925329d0e45f88ef2de
ildar habibrahmanov <ildar.habibrahmanov@gmail.com>
2020-10-05 13:58:38 +0700

    a: PHPMailer thankyou.html m:index.html _header.sass

--- js/main.js
+++ js/main.js
@@ -1,84 +1,113 @@
-//swiper slider for hotel section
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+$(document).ready(function () {
+  //swiper slider for hotel section
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button-next',
-    prevEl: '.hotel-slider__button-prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-
-// swiper slider for review section
-var reviewSlider = new Swiper('.reviews-slider', {
-  direction: 'horizontal',
-  loop: true,
-  navigation: {
-    nextEl: '.reviews-slider__button-next',
-    prevEl: '.reviews-slider__button-prev',
-  },
-});
-
-// yandex map
-ymaps.ready(init);
-function init() {
-  let myPlacemark;
-  var myMap = new ymaps.Map('map', {
-    center: [37.78526803249929, -122.4082276146421],
-    zoom: 16,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button-next',
+      prevEl: '.hotel-slider__button-prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
   });
 
-  (myPlacemark = new ymaps.Placemark([37.78526803249929, -122.4082276146421])),
+  // swiper slider for review section
+  var reviewSlider = new Swiper('.reviews-slider', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.reviews-slider__button-next',
+      prevEl: '.reviews-slider__button-prev',
+    },
+  });
+
+  // yandex map
+  ymaps.ready(init);
+  function init() {
+    let myPlacemark;
+    var myMap = new ymaps.Map('map', {
+      center: [37.78526803249929, -122.4082276146421],
+      zoom: 16,
+    });
+
+    (myPlacemark = new ymaps.Placemark([37.78526803249929, -122.4082276146421])),
+      {
+        hintContent: 'Hotel',
+        balloonContent: 'Hilton Grand Hotel',
+      };
+    myMap.geoObjects.add(myPlacemark);
+  }
+
+  // parallax newsletter section
+
+  function isElementInViewport(el) {
+    let rect = el.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.pageYOffset || document.documentElement.clientHeight) /* or $(window).height() */ &&
+      rect.right <=
+        (window.pageXOffset || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+  }
+
+  const box = document.querySelector('.newsletter'),
+    URL_BG_PATH = 'img/newsletter_background.jpg';
+
+  document.addEventListener(
+    'scroll',
+    function () {
+      let scrollValue = window.pageYOffset;
+      // console.log(messageText);
+      if (isElementInViewport(box)) {
+        box.style.background = 'url(' + URL_BG_PATH + ')';
+        box.style.backgroundPositionY = scrollValue * 0.1 + '%';
+      } else {
+        // box.style.background = '';
+      }
+    },
     {
-      hintContent: 'Hotel',
-      balloonContent: 'Hilton Grand Hotel',
-    };
-  myMap.geoObjects.add(myPlacemark);
-}
-
-// parallax newsletter section
-
-function isElementInViewport(el) {
-  let rect = el.getBoundingClientRect();
-
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.pageYOffset || document.documentElement.clientHeight) /* or $(window).height() */ &&
-    rect.right <=
-      (window.pageXOffset || document.documentElement.clientWidth) /* or $(window).width() */
+      passive: true,
+    },
   );
-}
 
-const box = document.querySelector('.newsletter'),
-  URL_BG_PATH = 'img/newsletter_background.jpg';
+  // mobile menu
+  let menuButton = $('.menu-button');
+  menuButton.on('click', function () {
+    $('.navbar-bottom').toggleClass('navbar-bottom_visible');
+  });
 
-document.addEventListener(
-  'scroll',
-  function () {
-    let scrollValue = window.pageYOffset;
-    // console.log(messageText);
-    if (isElementInViewport(box)) {
-      box.style.background = 'url(' + URL_BG_PATH + ')';
-      box.style.backgroundPositionY = scrollValue * 0.1 + '%';
-    } else {
-      // box.style.background = '';
+  let modalButton = $('[data-toggle=modal]'),
+    closeModalButton = $('.modal__close');
+
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    let modalOverlay = $('.modal__overlay'),
+      modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay_visible');
+    modalDialog.addClass('modal__dialog_visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    let modalOverlay = $('.modal__overlay'),
+      modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay_visible');
+    modalDialog.removeClass('modal__dialog_visible');
+  }
+
+  $(document).keydown(function (event) {
+    if (event.keyCode == 27) {
+      closeModal(event);
     }
-  },
-  {
-    passive: true,
-  },
-);
-
-// mobile menu
-let menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom_visible');
+  });
 });
