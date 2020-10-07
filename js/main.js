297e986a44cc0b778850633ca77d3d28942583b2
ildar habibrahmanov <ildar.habibrahmanov@gmail.com>
2020-10-07 13:50:09 +0700

    minify css svg js img

--- js/main.js
+++ js/main.js
@@ -27,58 +27,6 @@ $(document).ready(function () {
     },
   });
 
-  // yandex map
-  ymaps.ready(init);
-  function init() {
-    let myPlacemark;
-    var myMap = new ymaps.Map('map', {
-      center: [37.78526803249929, -122.4082276146421],
-      zoom: 16,
-    });
-
-    (myPlacemark = new ymaps.Placemark([37.78526803249929, -122.4082276146421])),
-      {
-        hintContent: 'Hotel',
-        balloonContent: 'Hilton Grand Hotel',
-      };
-    myMap.geoObjects.add(myPlacemark);
-  }
-
-  // parallax newsletter section
-
-  // function isElementInViewport(el) {
-  //   let rect = el.getBoundingClientRect();
-
-  //   return (
-  //     rect.top >= 0 &&
-  //     rect.left >= 0 &&
-  //     rect.bottom <=
-  //       (window.pageYOffset || document.documentElement.clientHeight) /* or $(window).height() */ &&
-  //     rect.right <=
-  //       (window.pageXOffset || document.documentElement.clientWidth) /* or $(window).width() */
-  //   );
-  // }
-
-  // const box = document.querySelector('.newsletter'),
-  //   URL_BG_PATH = 'img/newsletter_background.jpg';
-
-  // document.addEventListener(
-  //   'scroll',
-  //   function () {
-  //     let scrollValue = window.pageYOffset;
-  //     // console.log(messageText);
-  //     if (isElementInViewport(box)) {
-  //       box.style.background = 'url(' + URL_BG_PATH + ')';
-  //       box.style.backgroundPositionY = scrollValue * 0.1 + '%';
-  //     } else {
-  //       // box.style.background = '';
-  //     }
-  //   },
-  //   {
-  //     passive: true,
-  //   },
-  // );
-
   $('.parallax-window').parallax({
     naturalWidth: 2880,
     naturalHeight: 1913,
