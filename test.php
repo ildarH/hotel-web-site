14845106176187331f74ae7ca7718183f1543156
ildar habibrahmanov <ildar.habibrahmanov@gmail.com>
2020-11-30 20:34:53 +0700

    final

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$smtp_server = include 'mailconfig.php';
-
-echo $smtp_server['host'];
-echo $smtp_server['tcp_port'];
-echo $smtp_server['ssl_port'];
-echo $smtp_server['login'];
-echo $smtp_server['password'];
